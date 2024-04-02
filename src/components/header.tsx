e736814a16511a50093d4fe666b9e6ba07b45c50
tashiro <andre_seichi@hotmail.com>
2024-04-02 01:36:39 -0300

    feat: background blur ao mover tela

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,11 +6,19 @@ import { NavigationMenuHeader } from "./navigation-menu-header";
 import { buttonVariants } from "./ui/button";
 
 import { cn } from "@/lib/utils";
+import { useScrollPosition } from "@/hooks/useScrollPosition";
 
 export function Header() {
+  const scrollPosition = useScrollPosition();
+
   return (
-    <header className="sticky left-0 right-0 top-0 z-50 border-b bg-background">
-      <div className="flex h-16 items-center px-32">
+    <header
+      className={cn(
+        "fixed left-0 right-0 top-0 z-50",
+        scrollPosition > 0 ? "backdrop-blur-sm border-b" : "bg-transparent",
+      )}
+    >
+      <div className="flex h-20 items-center px-32">
         <div className="flex items-center space-x-10">
           <h1>Muad&apos;dib</h1>
 

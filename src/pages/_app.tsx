86a37689c7677770532931d6902df71a7082f15c
tashiro <andre_seichi@hotmail.com>
2024-04-02 00:43:26 -0300

    feat: theme provider e font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Inter as FontSans } from "next/font/google";
+
+import { ThemeProvider } from "@/components/theme-provider";
+
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
+
+const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <main className={cn("font-sans", fontSans.variable)}>
+        <Component {...pageProps} />
+      </main>
+    </ThemeProvider>
+  );
 }

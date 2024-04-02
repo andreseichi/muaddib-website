e78ee7a689b9df3e840bd04294ced7ca5fa2880a
tashiro <andre_seichi@hotmail.com>
2024-04-02 00:42:47 -0300

    feat: componente shadcn navigation menu

---
+++ src/components/navigation-menu.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
+
+export function NavigationMenuComponent() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Navegação</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="flex flex-col space-y-1 w-[200px] p-4">
+              <ListItem title="Home" href="#home" />
+              <ListItem title="Sobre nós" href="#about" />
+              <ListItem title="Nossa equipe" href="#team" />
+              <ListItem title="Fale conosco" href="#contact" />
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className,
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";

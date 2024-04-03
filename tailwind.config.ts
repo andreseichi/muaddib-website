646baea849aa3480ef26d7476c1ed12e139aa182
tashiro <andre_seichi@hotmail.com>
2024-04-02 21:17:30 -0300

    feat: animação tilt

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -72,10 +72,22 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        tilt: {
+          "0%, 50%, 100%": {
+            transform: "rotate(0deg)",
+          },
+          "25%": {
+            transform: "rotate(0.1deg)",
+          },
+          "75%": {
+            transform: "rotate(-0.1deg)",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        tilt: "tilt 10s infinite linear",
       },
     },
   },
